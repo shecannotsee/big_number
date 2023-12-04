33e28321e215478a783340da492b4f2a1eeaf441
shecannotsee <shecannotsee@163.com>
2023-12-04 16:27:57 +0800

    [feat] init

---
+++ cmake/big_number-config.cmake
@@ -0,0 +1,13 @@
+
+# ${big_number_FOUND}       : right get
+# ${big_number_INCLUDE_DIR} : include
+# ${big_number_LIBRARIES}   : lib
+# ${big_number_LINK_TARGET} : link target
+
+set(big_number "big_number")
+set(${big_number}_FOUND "1")
+set(${big_number}_INCLUDE_DIR   "${CMAKE_PREFIX_PATH}/include")
+set(${big_number}_LIBRARIES     "${CMAKE_PREFIX_PATH}/lib")
+set(${big_number}_LINK_TARGET   "big_number")
+
+
